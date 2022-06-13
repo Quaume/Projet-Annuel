520b1e8827552952ed8fd9683a9bfd48363bf0c7
Quaume <comaloisi@gmail.com>
2022-06-13 13:22:47 +0200

    rework fonctions

--- html/utrack_forum/functions/php-config/config.inc.php
+++ html/utrack_forum/functions/php-config/config.inc.php
@@ -1,8 +1,8 @@
 <?php
 //Création de toutes les constantes de mon projet
 define("DB_DRIVER", "mysql");
-define("DB_USER", "yvann");
-define("DB_PWD", "yvann");
+define("DB_USER", "root");
+define("DB_PWD", "Lamartine0");
 define("DB_HOST", "localhost");
 define("DB_PORT", "3306");
 define("DB_NAME", "utrackpa");
