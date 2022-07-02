47afe5568328dd710aa6a0a5210d60a4aed66034
Quaume <comaloisi@gmail.com>
2022-07-02 19:53:48 +0200

    getalltracksbytype

--- html/functions/functions.php
+++ html/functions/functions.php
@@ -295,6 +295,17 @@ function getRecentUserTracksById($id){
 
 }
 
+// All tracks by type
+
+function getAllTracksByType($type){
+	
+	$pdo = connectDB();
+	$queryPrepared = $pdo->prepare("SELECT id,title,artist,category,dateOfRelease,img_profile,trackName FROM utrackpa_tracks WHERE category = $type");	
+	$queryPrepared->execute();
+	
+	return $queryPrepared->fetchAll();
+}
+
 // All username
 function getAllTracks(){
 	
