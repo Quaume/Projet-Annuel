9371d57289397cfdff47bd5a6db55c194a8e21a5
Wany <Yvannkakou03@gmail.com>
2022-07-03 19:03:38 +0200

    push

--- html/templates/Home/lightDarkMode.js
+++ html/templates/Home/lightDarkMode.js
@@ -1,6 +1,14 @@
 const body = document.querySelector("body"),
-        switchMode = body.querySelector(".form-check-input");
-        
-        switchMode.addEventListener("click", () => {
-            body.classList.toggle("light");
-        });
+switchMode = body.querySelector(".form-check-input");
+
+switchMode.addEventListener("click", () => {
+    if(localStorage.getItem("light") === "false"){
+        localStorage.setItem("light","true");
+        body.classList.toggle("light");
+    }else {
+        localStorage.getItem("light") === "true";
+        localStorage.removeItem("light");
+        body.classList.toggle.remove("light");
+    }
+});
+ 
